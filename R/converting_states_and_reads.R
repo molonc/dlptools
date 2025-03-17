81d5dfd120472437c46d6e30bae50540f6e231f6
benjaminfurman <benjamin.ls.furman@gmail.com>
2025-03-17 14:07:50 -0700

    feat: separate segments into bins.

--- R/converting_states_and_reads.R
+++ R/converting_states_and_reads.R
@@ -41,3 +41,71 @@ rle_states <- function(states) {
   rles <- base::rep(base::seq_along(states_rle$lengths), states_rle$lengths)
   return(rles)
 }
+
+
+#' split segments into bins of a requested size.
+#'
+#' Takes a dataframe with segment start and end columns and returns a dataframe
+#' with those segments split into individual bins. All segment information
+#' applied to the generated bins.
+#'
+#' Warnings issues if requested bin size is bigger than some segments or if
+#' segments can't be split evenly into the bins.
+#'
+#' Bin end will not exceed a segment end. Depending on what is input and
+#' requested, this can lead to one smaller bin at the end of the segment.
+#'
+#' @param segs_df dataframe (or similar) of segment dat
+#' @param bin_size width of bins to split into. Default is standard 500kb
+#' @param seg_start_col name of the column that indicates the start of a segment
+#' @param seg_end_col name of the column that indicates the end of a segment
+#' @importFrom rlang .data
+#' @return input frame split into bins
+segs_to_reads <- function(
+    segs_df, bin_size = 5e5, seg_start_col = "start", seg_end_col = "end") {
+  binned_segs <- segs_df |>
+    dplyr::mutate(
+      seg_width = .data[[seg_end_col]] - .data[[seg_start_col]],
+      bins = purrr::map(seg_width, \(width) {
+        expand_length_to_bins(width, bin_size = bin_size) |>
+          dplyr::rename(bin_start = start, bin_end = end)
+      })
+    ) |>
+    tidyr::unnest(bins) |>
+    dplyr::mutate(
+      bin_start = dplyr::if_else(
+        bin_start == 1,
+        bin_start + .data[[seg_start_col]] - 1,
+        bin_start + .data[[seg_start_col]]
+      ),
+      bin_end = bin_end + .data[[seg_start_col]],
+      short_seg = seg_width < bin_size,
+      bin_end = dplyr::if_else(
+        # reset any bins that exceed end of segment
+        bin_end > .data[[seg_end_col]], .data[[seg_end_col]], bin_end
+      ),
+      uneven_bin = bin_end - bin_start + 1 < bin_size
+    ) |>
+    dplyr::rename(
+      seg_start = .data[[seg_start_col]],
+      seg_end = .data[[seg_end_col]],
+      start = bin_start,
+      end = bin_end
+    )
+
+  if (TRUE %in% unique(binned_segs$uneven_bin)) {
+    warning(paste0(
+      "Some segments unable to be evenly split into requested bin sizes.",
+      " Use df$uneven_bin column to find which ones."
+    ))
+  }
+
+  if (TRUE %in% unique(binned_segs$short_seg)) {
+    warning(paste0(
+      "Some segments shorter than the requested bin sizes.",
+      " Use df$short_seg column to find which ones."
+    ))
+  }
+
+  return(binned_segs)
+}
