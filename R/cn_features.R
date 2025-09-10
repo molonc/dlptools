381164392d398f029d3635d3d3be51cc3c9b5df4
benjaminfurman <benjamin.ls.furman@gmail.com>
2025-09-10 15:26:49 -0700

    feat: count oscillations

--- R/cn_features.R
+++ R/cn_features.R
@@ -11,7 +11,8 @@
 #' They employ 4 base features, that they cross for 90 categories:
 #' 1. CN states: 5 bins, 0-1, 2, 3, 4, 5+
 #' 2. segment size: 3 bins, <5 MB, 5-10Mb, 10Mb,
-#' 3. segment shape: 3 bins, LL (low left, low right segment), HH (high left, high right segment), OT (other)
+#' 3. segment shape: 3 bins, LL (low left, low right segment), HH (high left,
+#' high right segment), OT (other)
 #' 4. segment change: 2 bins: AA (difference between surrounding segments <=
 #' some critical value), or BB
 #'
@@ -31,7 +32,8 @@
 #' more than breaks. Defaults follow paper. Default is < 5Mb, 5--10Mb, > 10Mb
 #' specified as c(5e6, 10e6 + 1). Internally, base::cut() is used, so 2 splits
 #' produces 3 bins.
-#' @param annotate_input boolean. return input dataframe annotating each segment with the feature categories it falls into.
+#' @param annotate_input boolean. return input dataframe annotating each
+#' segment with the feature categories it falls into.
 #' @param return_matrix boolean. Return a cell-by-feature matrix of counts.
 #' @param ... can pass change_split_val to alter critical value for AA/BB split
 #' @return default return is a tibble of feature counts for each cell id.
@@ -535,4 +537,82 @@ extract_breakpoints <- function(
   )
 
   return(bp_counts)
+}
+
+#' extract the legths of chains of osscilating copy number segments
+#'
+#' Osscilating segments of copy numbers take from of N-M-N, e.g., 3-4-3. The
+#' length of these chains reflects the number of oscillations. Chains are
+#' evaluated in 3-segment intervals (the minimum required to recognize an
+#' oscillation). Chromosomse with < 3 segments cannot have oscillations, and
+#' will receive a 0.
+#'
+#' Chromosomes are evaluated in 3-segments sliding windows, with a window
+#' receiving a 1 if the 3-set is an oscillation, and a 0 if not. Adjacent
+#' oscillation sets are summed, non-osscilating sets are left as 0.
+#'
+#' Examples of Chain : length
+#' * 2-3-4: 0
+#' * 3-4-3 : 1
+#' * 3-4-3-4: 2
+#' * 3-4-3-2: 1 0 (3-4-3 is a chain, 4-3-2 is not)
+#' * 2-3-4-5: 0 0
+#'
+#' An early paper arguing for the use of oscillations to detect chromothripsis
+#' outlined that oscillations should be within 1 or 2 CN values. Using N-M-N
+#' notation from above, this would mean that Ns should be the same value and M
+#' would be within 1 or 2 copy values of N. Some pubilications relax these
+#' constraints, and for example allow M to be any value other than N. This
+#' means that 3-300-3 is counted as an oscillation by several publications.
+#' Granted, this is an oscillation, but not likely to be one indicative of
+#' chromothripsis.
+#'
+#' Others allow some flexiblity in the matching of the first and second N,
+#' allowing them to be within some margin of difference. Componding these two
+#' things leads to fairly extreme senarios, with some methods allowing chains
+#' like 5-300-6 to count as an oscillation.
+#'
+#' These behaviours can be replicated here, but isn't recommended. By default,
+#' Ns must match exactly, and M must be within 2 CNs.
+#'
+#' @param segs_df dataframe. Copy number segments for samples.
+#' @param middle_bound integer. How many copy number values away the middle
+#' segment can be from the end segments to count as an oscillation. Default 2
+#' (e.g., 3-4-3 and 3-5-3 both count as oscillations, but 3-7-3 would not).
+#' @param ends_bound integer. How many CN values apart the two ends of a
+#' 3-segment set are allow to be to count as an oscillation. Default is 0,
+#' i.e., they must be the same copy number.
+#' @param sample_col string. Name of column with cell/sample names
+#' @param chrom_col string. Name of column with chromosome names
+#' @param cn_col string. Name of column with segment copy number states.
+#' @return dataframe. sample IDs and observed chain lengths.
+#' @export
+extract_oscillations <- function(
+    segs_df,
+    middle_bound = 2,
+    ends_bound = 0,
+    sample_col = "cell_id",
+    chrom_col = "chr",
+    cn_col = "state") {
+  # TODO: this is pretty gross. Is it really better that filtering,
+  # splitting, or something else?
+  res <- segs_df %>%
+    split(.[[sample_col]]) |>
+    purrr::map(
+      .x = _, \(x) split(x[[cn_col]], x[[chrom_col]])
+    ) |>
+    purrr::map(\(samp, samp_name) {
+      purrr::map(samp, \(chrom) {
+        count_oscillations(chrom)
+      })
+    })
+
+  sample_osc_counts <- res |>
+    purrr::list_flatten(name_spec = "{outer}&&&{inner}") |>
+    tibble::enframe() |>
+    tidyr::unnest_longer(value) |>
+    tidyr::separate_wider_delim(name, names = c(sample_col, "chrom"), "&&&") |>
+    dplyr::select(dplyr::any_of(sample_col), OSc = value)
+
+  return(sample_osc_counts)
 }
